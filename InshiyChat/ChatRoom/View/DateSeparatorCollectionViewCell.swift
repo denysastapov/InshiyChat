0ff7c8496abb10ebf2b3fa8d4a8aef72e98fc419
denysastspov <purple.rain1982@gail.com>
2024-01-30 15:22:29 +0200

    Added DatabaseManager, rewrited Friends screen according to the existing databaseManager, small fixes and error handling

---
+++ InshiyChat/ChatRoom/View/DateSeparatorCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  DateSeparatorCollectionViewCell.swift
+//  InshiyChat
+//
+//  Created by Denys Astapov on 18.01.2024.
+//
+
+import Foundation
---
+++ InshiyChat/DatabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManager.swift
+//  InshiyChat
+//
+//  Created by Denys Astapov on 19.01.2024.
+//
+
+import Foundation
